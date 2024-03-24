49fed86d8eb00946aae3240b145d35aea9b83c2d
LuisUcla <caluigi7@gmail.com>
2024-03-23 22:42:53 -0300

    refactor: change app-component para mostrar las vistas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductsService } from '@api/products.service';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, JsonPipe],
+  imports: [CommonModule, RouterOutlet, JsonPipe, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  private readonly productsService = inject(ProductsService)
-  products = this.productsService.products; // signal []
-}
+export class AppComponent {}
