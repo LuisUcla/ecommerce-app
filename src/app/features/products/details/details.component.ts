7fdc45ff235892611f4e4d964b7c07d0838cf95a
LuisUcla <caluigi7@gmail.com>
2024-04-04 16:11:27 -0300

    feat: add interpolacion and inputBinding

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, OnInit, Signal, inject, input } from '@angular/core';
+import { ProductsService } from '@api/products.service';
+import { Product } from '@shared/models/products.interface';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export default class DetailsComponent {
+export default class DetailsComponent implements OnInit {
+  productId = input<number>(0, { alias: 'id' })
+  product!: Signal<Product | undefined>
+  private readonly productService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.product = this.productService.getProductById(this.productId())
+  }
   onAddToCart() {}
 }
