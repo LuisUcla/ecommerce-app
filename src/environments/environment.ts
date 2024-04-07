ff81de40f6d188afadce4acf21c7743b00c6cfeb
LuisUcla <caluigi7@gmail.com>
2024-04-07 13:05:49 -0300

    refactor: change keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: 'Https://fakestoreapi.com',
-    stripeApiKey: '',
+    stripeApiKey: '', // --> pk_test_xxxxxxxxxxx
     serverUrl: 'http://localhost:4242'
 };
